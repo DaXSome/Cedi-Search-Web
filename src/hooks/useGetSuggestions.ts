4fea9c353f18453c41c0762c04ee467325b51c91
owbird <owbirdphorccuo@gmail.com>
2023-11-15 04:48:04 +0000

    Chore (Get Suggestions): Optimize request

--- src/hooks/useGetSuggestions.ts
+++ src/hooks/useGetSuggestions.ts
@@ -4,14 +4,14 @@ const useGetSuggestions = (query: string) => {
   const [suggestions, setSuggestions] = useState<string[]>([]);
 
   const getSuggestions = async () => {
-    const res = await fetch(
-      `http://0.0.0.0:8000?query=${query}`,
-      // `https://cedi-search-search-suggestion-service.vercel.app?query=${query}`,
-      { cache: "no-cache" },
-    );
+    try {
+      const res = await fetch(`http://0.0.0.0:8000?query=${query}`, {
+        cache: "no-cache",
+      });
 
-    const suggestions = await res.json();
-    setSuggestions(suggestions);
+      const suggestions = await res.json();
+      setSuggestions(suggestions);
+    } catch (error) {}
   };
 
   useEffect(() => {
