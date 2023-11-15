2ac900d8c07ad45574a3760d3e25feb6dfc4de43
owbird <owbirdphorccuo@gmail.com>
2023-11-15 05:41:44 +0000

    refactor

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,21 +4,25 @@ import ResultCard from "@/components/search/ResultCard";
 import { ISearchResponse } from "@/data/interfaces";
 import { Fragment } from "react";
 
-const Search = async (params: { searchParams: { query: string } }) => {
+interface IParams {
+  searchParams: { query: string };
+}
+
+const Search = async (params: IParams) => {
   const { query } = params.searchParams;
 
   const req = await fetch(`http://localhost:8080/search?query=${query}`, {
     cache: "no-cache",
   });
 
-  const results = (await req.json()) as ISearchResponse;
+  const matches = (await req.json()) as ISearchResponse;
 
   return (
     <Fragment>
       <SearchBox />
 
       <div className="mt-5 grid grid-cols-3 gap-2">
-        {results.results.map((result) => (
+        {matches.results.map((result) => (
           <ResultCard key={result.product_id} {...result} />
         ))}
       </div>
