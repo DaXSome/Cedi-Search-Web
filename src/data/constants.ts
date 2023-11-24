5e7f48e8bd79d9e4e6075c5e7ef80f900c7ac220
owbird <owbirdphorccuo@gmail.com>
2023-11-24 10:03:15 +0000

    Fix (Constants): Fix production host url

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,7 +1,7 @@
 export const CURRENT_HOST =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://cedi-search-web.vercel.app";
+    : "https://cedi-search.vercel.app/";
 
 export const SEARCH_SUGGESTION_SERVICE_URL =
   "https://cedi-search-search-suggestion-service.vercel.app";
