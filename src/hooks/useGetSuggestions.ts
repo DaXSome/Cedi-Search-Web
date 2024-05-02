509b1b22080e25ab963cbe3e78e2147b0833d417
owbird <owbirdphorccuo@gmail.com>
2024-05-02 22:40:32 +0000

    perf (Suggestions): Cancel previous requests

--- src/hooks/useGetSuggestions.ts
+++ src/hooks/useGetSuggestions.ts
@@ -3,15 +3,14 @@ import { useEffect, useState } from "react";
 
 const useGetSuggestions = (query: string) => {
   const [suggestions, setSuggestions] = useState<string[]>([]);
+  const controller = new AbortController();
 
   const getSuggestions = async () => {
     try {
-      const res = await fetch(
-        `${API_URL}?query=${query}`,
-        {
-          cache: "no-cache",
-        },
-      );
+      const res = await fetch(`${API_URL}?query=${query}`, {
+        cache: "no-cache",
+        signal: controller.signal,
+      });
 
       const suggestions = await res.json();
       setSuggestions(suggestions);
@@ -24,6 +23,8 @@ const useGetSuggestions = (query: string) => {
     } else {
       setSuggestions([]);
     }
+
+    return () => controller.abort();
   }, [query]);
 
   return { suggestions };
