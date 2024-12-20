00f494fcfd51fa4bba756a59213e23c0afe857a8
owbird <owbirdphorccuo@gmail.com>
2024-12-20 11:17:40 +0000

    Refactor (product-page): Use tailwind

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -16,7 +16,41 @@ const ProductDetailsPage = async (params: IParams) => {
 
   const product = (await res.json()) as IProduct;
 
-  return <></>;
+  return (
+    <div className="container mx-auto max-w-7xl p-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 py-12">
+        <img src={product.images[0]} />
+
+        <div className="space-y-6">
+          <header>
+            <h1 className="text-2xl sm:text-4xl lg:text-5xl font-bold leading-tight">
+              {product.name}
+            </h1>
+            <p className="text-2xl font-light text-gray-900 dark:text-gray-400">
+              {product.price} GHS
+            </p>
+          </header>
+
+          <div className="space-y-4">
+            <div>
+              <h2 className="text-lg lg:text-xl font-semibold text-yellow-500 dark:text-yellow-300 uppercase mb-4">
+                Product Details
+              </h2>
+              <p>{product.description}</p>
+            </div>
+          </div>
+
+          <a
+            target="_blank"
+            href={product.url}
+            className="block w-full mt-8 text-center rounded-none py-3 bg-gray-900 text-white dark:bg-gray-50 dark:text-gray-900 uppercase font-medium hover:translate-y-1 hover:shadow-lg transition-transform"
+          >
+            Visit website
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
