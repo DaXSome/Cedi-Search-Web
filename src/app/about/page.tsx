85ed07662b018126330d37be60ecb6b6d44f1293
owbird <owbirdphorccuo@gmail.com>
2024-12-21 14:32:39 +0000

    feat: Add about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,58 @@
 const About = () => {
-  return <></>;
+  return (
+    <div className="p-8 max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Welcome to Cedi Search</h1>
+      <p className="text-lg mb-6">
+        Where the world of e-commerce unfolds at your fingertips! We understand
+        the dynamic and ever-expanding nature of online shopping, and
+        that&apos;s why we&apos;ve crafted a search engine tailored exclusively
+        for the e-commerce enthusiast.
+      </p>
+      <p className="text-lg mb-6">
+        Our mission is simple: to connect you with the products you love from
+        the vast landscape of e-commerce platforms. Whether you&apos;re hunting
+        for the latest fashion trends, tech gadgets, home decor, or anything in
+        between, we&apos;ve got you covered. Our advanced search algorithms sift
+        through the digital aisles of your favorite online stores, bringing you
+        the most relevant and up-to-date results.
+      </p>
+      <h2 className="text-xl font-semibold mb-4">Why choose Cedi Search?</h2>
+      <ul className="list-disc list-inside mb-6">
+        <li>
+          <strong>Specialized E-commerce Focus:</strong> We are dedicated
+          exclusively to e-commerce, ensuring that your search experience is
+          finely tuned to the intricacies of online shopping.
+        </li>
+        <li>
+          <strong>Comprehensive Product Coverage:</strong> From niche markets to
+          mainstream retailers, we scan the entire spectrum of e-commerce
+          platforms, ensuring you have access to a diverse array of products.
+        </li>
+        <li>
+          <strong>Real-Time Updates:</strong> Stay in the loop with real-time
+          updates on product availability, prices, and promotions. Never miss
+          out on a great deal again!
+        </li>
+        <li>
+          <strong>Smart Recommendations:</strong> Discover new and exciting
+          products with our intelligent recommendation engine. Personalized
+          suggestions make your shopping journey more enjoyable.
+        </li>
+        <li>
+          <strong>Secure and Trustworthy:</strong> Your privacy and security are
+          our top priorities. Shop with confidence, knowing that we prioritize
+          your data protection.
+        </li>
+      </ul>
+      <p className="text-lg">
+        Whether you&apos;re a seasoned online shopper or just embarking on your
+        e-commerce adventure, Cedi Search is here to simplify and enhance your
+        search for the perfect find. Join us in exploring the world of
+        e-commerce like never before!
+      </p>
+    </div>
+  );
 };
 
 export default About;
+
